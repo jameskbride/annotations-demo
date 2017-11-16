48cfd1650ece03b28fad9a019fa2578e7dfd4b61
Jim Kirkbride <james.kirkbride@gmail.com>
2017-11-15 19:57:00 -0500

    Removing dead code.

--- example/src/main/java/com/jameskbride/example/model/SomeObject.java
+++ example/src/main/java/com/jameskbride/example/model/SomeObject.java
@@ -24,17 +24,9 @@ public class SomeObject implements Serializable {
         return stringValue;
     }
 
-    public void setStringValue(String stringValue) {
-        this.stringValue = stringValue;
-    }
-
     @MethodMarker(methodName = "Method 2")
     public Integer getIntegerValue() {
         return integerValue;
-    }
-
-    public void setIntegerValue(Integer integerValue) {
-        this.integerValue = integerValue;
     }
 
     @Override
