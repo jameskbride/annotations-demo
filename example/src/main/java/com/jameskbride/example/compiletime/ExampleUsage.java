1fca2bd9ce3d70039f85ef9c0cbbdc46f51b08ce
Jim Kirkbride <james.kirkbride@gmail.com>
2017-11-15 20:56:11 -0500

    Refactoring.

--- example/src/main/java/com/jameskbride/example/compiletime/ExampleUsage.java
+++ example/src/main/java/com/jameskbride/example/compiletime/ExampleUsage.java
@@ -14,7 +14,9 @@ public class ExampleUsage {
 
     public static void main(String[] args) {
         ExampleApi exampleApi = new ExampleApiProxy();
+
         Call<SomeObject> call = exampleApi.getSomeObject();
+
         call.enqueue(new Callback<SomeObject>() {
             @Override
             public void onFailure(okhttp3.Call call, IOException e) {
@@ -26,20 +28,23 @@ public class ExampleUsage {
             public void onResponse(okhttp3.Call call, Response<SomeObject> response) throws IOException {
                 SomeObject body = response.body();
                 SomeObjectProcessor someObjectProcessor = new SomeObjectProcessor(body);
-                System.out.println("Got the response: " + body.toString());
-                System.out.println("Class marker: " + someObjectProcessor.getClassMarkerValue());
-                try {
-                    System.out.println("Class property 1: " + someObjectProcessor.getPropertyMarker("stringValue"));
-                    System.out.println("Class property 2: " + someObjectProcessor.getPropertyMarker("integerValue"));
-                    System.out.println("Class method 1: " + someObjectProcessor.getMethodMarker("getStringValue"));
-                    System.out.println("Class method 2: " + someObjectProcessor.getMethodMarker("getIntegerValue"));
-                } catch (NoSuchFieldException e) {
-                    e.printStackTrace();
-                } catch (NoSuchMethodException e) {
-                    e.printStackTrace();
-                }
-
+                printResponse(body, someObjectProcessor);
             }
         });
+    }
+
+    private static void printResponse(SomeObject body, SomeObjectProcessor someObjectProcessor) {
+        System.out.println("Got the response: " + body.toString());
+        System.out.println("Class marker: " + someObjectProcessor.getClassMarkerValue());
+        try {
+            System.out.println("Class property 1: " + someObjectProcessor.getPropertyMarker("stringValue"));
+            System.out.println("Class property 2: " + someObjectProcessor.getPropertyMarker("integerValue"));
+            System.out.println("Class method 1: " + someObjectProcessor.getMethodMarker("getStringValue"));
+            System.out.println("Class method 2: " + someObjectProcessor.getMethodMarker("getIntegerValue"));
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
     }
 }
