264faf0e2f2f3f45bf2cfeda1eaaea0c3bbb2a66
Jim Kirkbride <james.kirkbride@gmail.com>
2017-11-15 20:38:48 -0500

    Formatting.

--- example/src/main/java/com/jameskbride/example/runtime/SomeObjectProcessor.java
+++ example/src/main/java/com/jameskbride/example/runtime/SomeObjectProcessor.java
@@ -9,24 +9,28 @@ public class SomeObjectProcessor {
     private SomeObject someObject;
 
     public SomeObjectProcessor(SomeObject someObject) {
-
         this.someObject = someObject;
     }
 
     public String getClassMarkerValue() {
         ClassMarker classMarker = someObject.getClass().getAnnotation(ClassMarker.class);
+
         return classMarker.className();
     }
 
     public String getPropertyMarker(String propertyName) throws NoSuchFieldException {
         Field stringValueField = someObject.getClass().getDeclaredField(propertyName);
+
         PropertyMarker annotation = stringValueField.getAnnotation(PropertyMarker.class);
+
         return annotation.propertyName();
     }
 
     public String getMethodMarker(String methodName) throws NoSuchMethodException {
         Method stringValueField = someObject.getClass().getMethod(methodName);
+
         MethodMarker annotation = stringValueField.getAnnotation(MethodMarker.class);
+
         return annotation.methodName();
     }
 }
